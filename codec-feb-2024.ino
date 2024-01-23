ae2d391628009a7e2825cbc7310d41b7cb2ab7b8
Bregy Malpartida <bregy@minsky.cc>
2024-01-22 22:04:10 -0500

    feat: remove SimpleKalmanFilter and unused variables

--- codec-feb-2024.ino
+++ codec-feb-2024.ino
@@ -1,131 +1,153 @@
 #include <ESP_8_BIT_composite.h>
-#include <SimpleKalmanFilter.h>
+// #include <SimpleKalmanFilter.h>
 
-const uint8_t input_pot_1_pin = 34;
-const uint8_t input_pot_2_pin = 35;
-const uint8_t input_pot_3_pin = 32;
-const uint8_t input_pot_4_pin = 33;
+// const uint8_t input_pot_1_pin = 34;
+// const uint8_t input_pot_2_pin = 35;
+// const uint8_t input_pot_3_pin = 32;
+// const uint8_t input_pot_4_pin = 33;
 
-const int CELL_SIZE_X = 2;
-const int CELL_SIZE_Y = 2;
+// const int CELL_SIZE_X = 2;
+// const int CELL_SIZE_Y = 2;
 
-const int PIXELS_X = 256;
-const int PIXELS_Y = 240;
+// const int PIXELS_X = 256;
+// const int PIXELS_Y = 240;
 
-const int CELL_LIFETIME = 7;
+// const int CELL_LIFETIME = 7;
 
-const int CELLS_X = PIXELS_X / CELL_SIZE_X;
-const int CELLS_Y = PIXELS_Y / CELL_SIZE_Y;
+// const int CELLS_X = PIXELS_X / CELL_SIZE_X;
+// const int CELLS_Y = PIXELS_Y / CELL_SIZE_Y;
 
-const uint8_t STATE_DEAD = 0;
-const uint8_t STATE_ALIVE = CELL_LIFETIME - 1;
+// const uint8_t STATE_DEAD = 0;
+// const uint8_t STATE_ALIVE = CELL_LIFETIME - 1;
 
 ESP_8_BIT_composite video_out(true);
 
-SimpleKalmanFilter filter_1(1, 1, 0.01);
+// SimpleKalmanFilter filter_1(1, 1, 0.01);
 
-uint16_t born_rule = 0b000001000;     // {3}
-uint16_t survive_rule = 0b000001100;  // {3,2}
+// uint16_t born_rule = 0b000001000;     // {3}
+// uint16_t survive_rule = 0b000001100;  // {3,2}
 
-uint8_t board[CELLS_Y * CELLS_X];
-uint8_t board_copy[CELLS_Y * CELLS_X];
+uint8_t screen[256 * 240];
+// uint8_t board_copy[CELLS_Y * CELLS_X];
 
-void render(uint8_t** frameBufferLines, int color_multiplier) {
-  for (int y = 0; y < PIXELS_Y; y++) {
-    for (int x = 0; x < PIXELS_X; x++) {
-      int index = (y / CELL_SIZE_Y) * CELLS_Y + (x / CELL_SIZE_X);
+// void render(uint8_t** frameBufferLines, int color_multiplier) {
+//   for (int y = 0; y < PIXELS_Y; y++) {
+//     for (int x = 0; x < PIXELS_X; x++) {
+//       int index = (y / CELL_SIZE_Y) * CELLS_Y + (x / CELL_SIZE_X);
 
-      frameBufferLines[y][x] = board[index] * color_multiplier / CELL_LIFETIME;
-    }
-  }
+//       frameBufferLines[y][x] = board[index] * color_multiplier /
+//       CELL_LIFETIME;
+//     }
+//   }
 
-  video_out.waitForFrame();
-}
+//   video_out.waitForFrame();
+// }
 
-void generate_center_line(uint8_t thickness) {
-  int y_from = CELLS_Y / 2 - 1 * thickness;
-  int y_to = CELLS_Y / 2 + 1 * thickness;
+// void generate_center_line(uint8_t thickness) {
+//   int y_from = CELLS_Y / 2 - 1 * thickness;
+//   int y_to = CELLS_Y / 2 + 1 * thickness;
 
-  for (int y = y_from; y < y_to; y++) {
-    for (int x = 0; x < CELLS_X; x++) {
-      board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
-    }
-  }
-}
+//   for (int y = y_from; y < y_to; y++) {
+//     for (int x = 0; x < CELLS_X; x++) {
+//       board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
+//     }
+//   }
+// }
+
+// uint8_t** frame_buffer;
 
 void setup() {
-  analogReadResolution(9);
+  // analogReadResolution(9);
 
   video_out.begin();
 
-  for (int y = 0; y < CELLS_Y; y++) {
-    for (int x = 0; x < CELLS_X; x++) {
-      board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
+  uint8_t** frame_buffer = video_out.getFrameBufferLines();
+
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 256; x++) {
+      int index = y * 256 + x;
+      screen[index] = 255;
     }
   }
 }
 
 void loop() {
   uint8_t** frame_buffer = video_out.getFrameBufferLines();
-  int estimated_value = filter_1.updateEstimate(analogRead(input_pot_1_pin));
 
-  int color_multiplier = map(estimated_value, 0, 511, 0, 255);
+  // int estimated_value = filter_1.updateEstimate(analogRead(input_pot_1_pin));
 
-  born_rule = analogRead(input_pot_2_pin);
-  survive_rule = analogRead(input_pot_3_pin);
+  // int color_multiplier = map(estimated_value, 0, 511, 0, 255);
 
-  int center_line_force = map(analogRead(input_pot_4_pin), 0, 511, 0, 15);
+  // born_rule = analogRead(input_pot_2_pin);
+  // survive_rule = analogRead(input_pot_3_pin);
 
-  render(frame_buffer, color_multiplier);
+  // int center_line_force = map(analogRead(input_pot_4_pin), 0, 511, 0, 15);
 
-  evolve();
+  // render(frame_buffer, color_multiplier);
 
-  if (center_line_force > 0) {
-    generate_center_line(center_line_force);
-  }
+  // evolve();
 
-  delay(16);
-}
+  // if (center_line_force > 0) {
+  //   generate_center_line(center_line_force);
+  // }
 
-void evolve() {
-  memcpy(board_copy, board, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+  int y_0 = random(0, 240);
+  int x_0 = random(0, 255);
 
-  for (int y = 0; y < CELLS_Y; y++) {
-    for (int x = 0; x < CELLS_X; x++) {
-      int current_state = board[y * CELLS_Y + x];
+  int index_0 = y_0 * 256 + x_0;
+  screen[index_0] = 0;
 
-      uint8_t total_n = 0;
-
-      for (int dx = -1; dx < 2; dx++) {
-        for (int dy = -1; dy < 2; dy++) {
-          if (dx != 0 || dy != 0) {
-            int neighborX = (x + dx) % CELLS_X;
-            int neighborY = (y + dy) % CELLS_Y;
-
-            int neighbor_state = board[neighborY * CELLS_Y + neighborX];
-
-            if (neighbor_state == STATE_ALIVE) {
-              total_n += 1;
-            }
-          }
-        }
-      }
-
-      if (current_state == STATE_DEAD) {
-        if ((born_rule >> total_n) & 1) {
-          board_copy[y * CELLS_Y + x] = STATE_ALIVE;
-        }
-      } else if (current_state == STATE_ALIVE) {
-        if (!((survive_rule >> total_n) & 1)) {
-          board_copy[y * CELLS_Y + x] = STATE_DEAD;
-        }
-      } else {
-        board_copy[y * CELLS_Y + x] = current_state - 1;
-      }
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 256; x++) {
+      int index = y * 256 + x;
+      frame_buffer[y][x] = screen[index];
     }
   }
 
-  memcpy(board, board_copy, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+  video_out.waitForFrame();
 
-  return;
+  // delay(100);
 }
+
+// void evolve() {
+//   memcpy(board_copy, board, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+
+//   for (int y = 0; y < CELLS_Y; y++) {
+//     for (int x = 0; x < CELLS_X; x++) {
+//       int current_state = board[y * CELLS_Y + x];
+
+//       uint8_t total_n = 0;
+
+//       for (int dx = -1; dx < 2; dx++) {
+//         for (int dy = -1; dy < 2; dy++) {
+//           if (dx != 0 || dy != 0) {
+//             int neighborX = (x + dx) % CELLS_X;
+//             int neighborY = (y + dy) % CELLS_Y;
+
+//             int neighbor_state = board[neighborY * CELLS_Y + neighborX];
+
+//             if (neighbor_state == STATE_ALIVE) {
+//               total_n += 1;
+//             }
+//           }
+//         }
+//       }
+
+//       if (current_state == STATE_DEAD) {
+//         if ((born_rule >> total_n) & 1) {
+//           board_copy[y * CELLS_Y + x] = STATE_ALIVE;
+//         }
+//       } else if (current_state == STATE_ALIVE) {
+//         if (!((survive_rule >> total_n) & 1)) {
+//           board_copy[y * CELLS_Y + x] = STATE_DEAD;
+//         }
+//       } else {
+//         board_copy[y * CELLS_Y + x] = current_state - 1;
+//       }
+//     }
+//   }
+
+//   memcpy(board, board_copy, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+
+//   return;
+// }
