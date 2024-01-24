60365cbfddabb0b3c30c51f48e9bdada6284a94c
Bregy Malpartida <bregy@minsky.cc>
2024-01-23 22:38:49 -0500

    feat: add vector and __locale to cpp mappings, remove unused code, and optimize draw_fern function

--- codec-feb-2024.ino
+++ codec-feb-2024.ino
@@ -1,64 +1,17 @@
 #include <ESP_8_BIT_composite.h>
-// #include <SimpleKalmanFilter.h>
-
-// const uint8_t input_pot_1_pin = 34;
-// const uint8_t input_pot_2_pin = 35;
-// const uint8_t input_pot_3_pin = 32;
-// const uint8_t input_pot_4_pin = 33;
-
-// const int CELL_SIZE_X = 2;
-// const int CELL_SIZE_Y = 2;
-
-// const int PIXELS_X = 256;
-// const int PIXELS_Y = 240;
-
-// const int CELL_LIFETIME = 7;
-
-// const int CELLS_X = PIXELS_X / CELL_SIZE_X;
-// const int CELLS_Y = PIXELS_Y / CELL_SIZE_Y;
-
-// const uint8_t STATE_DEAD = 0;
-// const uint8_t STATE_ALIVE = CELL_LIFETIME - 1;
 
 ESP_8_BIT_composite video_out(true);
 
-// SimpleKalmanFilter filter_1(1, 1, 0.01);
-
-// uint16_t born_rule = 0b000001000;     // {3}
-// uint16_t survive_rule = 0b000001100;  // {3,2}
-
 uint8_t screen[256 * 240];
-// uint8_t board_copy[CELLS_Y * CELLS_X];
-
-// void render(uint8_t** frameBufferLines, int color_multiplier) {
-//   for (int y = 0; y < PIXELS_Y; y++) {
-//     for (int x = 0; x < PIXELS_X; x++) {
-//       int index = (y / CELL_SIZE_Y) * CELLS_Y + (x / CELL_SIZE_X);
-
-//       frameBufferLines[y][x] = board[index] * color_multiplier /
-//       CELL_LIFETIME;
-//     }
-//   }
-
-//   video_out.waitForFrame();
-// }
-
-// void generate_center_line(uint8_t thickness) {
-//   int y_from = CELLS_Y / 2 - 1 * thickness;
-//   int y_to = CELLS_Y / 2 + 1 * thickness;
-
-//   for (int y = y_from; y < y_to; y++) {
-//     for (int x = 0; x < CELLS_X; x++) {
-//       board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
-//     }
-//   }
-// }
 
 float_t y;
 float_t x;
 
 float_t y_n;
 float_t x_n;
+
+float_t alpha = 0.01;
+bool alpha_up = true;
 
 void setup() {
   analogReadResolution(10);
@@ -86,28 +39,45 @@ void setup() {
 void loop() {
   uint8_t** frame_buffer = video_out.getFrameBufferLines();
 
-  // int estimated_value = filter_1.updateEstimate(analogRead(input_pot_1_pin));
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 256; x++) {
+      int index = y * 256 + x;
+      screen[index] = 0x00;
+    }
+  }
 
-  // int color_multiplier = map(estimated_value, 0, 511, 0, 255);
+  // Serial.printf("x: %f, y: %f\n", x, y);
 
-  // born_rule = analogRead(input_pot_2_pin);
-  // survive_rule = analogRead(input_pot_3_pin);
+  draw_fern();
 
-  // int center_line_force = map(analogRead(input_pot_4_pin), 0, 511, 0, 15);
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 256; x++) {
+      int index = y * 256 + x;
+      frame_buffer[239 - y][x] = screen[index % (256 * 240)];
+    }
+  }
 
-  // render(frame_buffer, color_multiplier);
+  if (alpha_up) {
+    alpha += 0.008;
+  } else {
+    alpha -= 0.003;
+  }
 
-  // evolve();
+  if (alpha > 0.8) {
+    alpha_up = false;
+  } else if (alpha < 0.01) {
+    alpha_up = true;
+  }
 
-  // if (center_line_force > 0) {
-  //   generate_center_line(center_line_force);
-  // }
+  video_out.waitForFrame();
+}
 
-  for (int i = 0; i < 100; i++) {
+void draw_fern() {
+  uint8_t color = 0x0D;
+
+  for (int i = 0; i < 5000; i++) {
     int dice = random(0, 100);
     // int dice = map(analogRead(34), 0, 1023, 0, 100);
-
-    uint8_t color = 0xFF;
 
     float_t a, b, c, d, e, f, p1, p2, p3, p4;
 
@@ -124,20 +94,15 @@ void loop() {
       e = 0;
       f = 0;
 
-      // x_n = 0;
-      // y_n = 0.16 * y;
     } else if (dice < (p1 + p2) * 100) {
-      a = 0.85;
+      a = alpha;
       b = 0.04;
       c = -0.04;
       d = 0.85;
       e = 0;
       f = 1.6;
 
-      // x_n = 0.75 * x + 0.14 * y;
-      // x_n = 0.85 * x + 0.04 * y;
-      // y_n = -0.04 * x + 0.85 * y + 1.6;
-      color = 0xF4;
+      // color = 0xF4;
     } else if (dice < (p1 + p2 + p3) * 100) {
       a = 0.2;
       b = -0.26;
@@ -146,9 +111,7 @@ void loop() {
       e = 0;
       f = 1.6;
 
-      // x_n = 0.2 * x - 0.26 * y;
-      // y_n = 0.23 * x + 0.22 * y + 1.6;
-      color = 0xE8;
+      // color = 0xE8;
     } else {
       a = -0.15;
       b = 0.28;
@@ -157,9 +120,7 @@ void loop() {
       e = 0;
       f = 0.44;
 
-      // x_n = -0.15 * x + 0.28 * y;
-      // y_n = 0.26 * x + 0.24 * y + 0.44;
-      color = 0x0F;
+      // color = 0x0F;
     }
 
     x_n = a * x + b * y + e;
@@ -173,61 +134,7 @@ void loop() {
 
     int index_0 = y_int * 256 + x_int;
     screen[index_0] = color;
+
+    // color += 0x01;
   }
-
-  for (int y = 0; y < 240; y++) {
-    for (int x = 0; x < 256; x++) {
-      int index = y * 256 + x;
-      frame_buffer[y][x] = screen[index];
-    }
-  }
-
-  // Serial.printf("x: %f, y: %f\n", x, y);
-
-  video_out.waitForFrame();
-
-  // delay(10);
 }
-
-// void evolve() {
-//   memcpy(board_copy, board, sizeof(uint8_t) * CELLS_Y * CELLS_X);
-
-//   for (int y = 0; y < CELLS_Y; y++) {
-//     for (int x = 0; x < CELLS_X; x++) {
-//       int current_state = board[y * CELLS_Y + x];
-
-//       uint8_t total_n = 0;
-
-//       for (int dx = -1; dx < 2; dx++) {
-//         for (int dy = -1; dy < 2; dy++) {
-//           if (dx != 0 || dy != 0) {
-//             int neighborX = (x + dx) % CELLS_X;
-//             int neighborY = (y + dy) % CELLS_Y;
-
-//             int neighbor_state = board[neighborY * CELLS_Y + neighborX];
-
-//             if (neighbor_state == STATE_ALIVE) {
-//               total_n += 1;
-//             }
-//           }
-//         }
-//       }
-
-//       if (current_state == STATE_DEAD) {
-//         if ((born_rule >> total_n) & 1) {
-//           board_copy[y * CELLS_Y + x] = STATE_ALIVE;
-//         }
-//       } else if (current_state == STATE_ALIVE) {
-//         if (!((survive_rule >> total_n) & 1)) {
-//           board_copy[y * CELLS_Y + x] = STATE_DEAD;
-//         }
-//       } else {
-//         board_copy[y * CELLS_Y + x] = current_state - 1;
-//       }
-//     }
-//   }
-
-//   memcpy(board, board_copy, sizeof(uint8_t) * CELLS_Y * CELLS_X);
-
-//   return;
-// }
